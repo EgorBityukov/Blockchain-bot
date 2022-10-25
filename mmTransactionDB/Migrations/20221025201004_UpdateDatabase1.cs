1a15b7d21d26d76028b0a3414656f9ebd2435373
Y_Bityukov <y.bityukov@softteco.com>
2022-10-25 23:10:54 +0300

    Up

---
+++ mmTransactionDB/Migrations/20221025201004_UpdateDatabase1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mmTransactionDB.Migrations
+{
+    public partial class UpdateDatabase1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WalletAddress",
+                table: "mmTransactions",
+                newName: "SendWalletAddress");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RecieveWalletAddress",
+                table: "mmTransactions",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecieveWalletAddress",
+                table: "mmTransactions");
+
+            migrationBuilder.RenameColumn(
+                name: "SendWalletAddress",
+                table: "mmTransactions",
+                newName: "WalletAddress");
+        }
+    }
+}
