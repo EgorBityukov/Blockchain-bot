e00c1f04a8c4645d3c26e26a172cc68c0b87864a
stanislau_kudzelka <nuryelse@gmail.com>
2022-11-09 10:57:14 +0100

    sh scripts

--- run-cleanup.sh
+++ run-cleanup.sh
@@ -1,3 +1,4 @@
 docker container stop mm-bot
 docker container rm mm-bot
+docker pull flerry/mm-bot:latest
 docker run -itd -p 25060:25060 --name mm-bot flerry/mm-bot:latest cleanup
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 docker container stop mm-bot
 docker container rm mm-bot
+docker pull flerry/mm-bot:latest
 docker run -itd -p 25060:25060 --name mm-bot flerry/mm-bot:latest
 
 
